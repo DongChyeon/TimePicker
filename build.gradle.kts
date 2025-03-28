9df0686a249fbc25ebac7884a97e36a841ecc879
dongchyeon <kimdonghyun90766@gmail.com>
2025-03-28 11:57:29 +0900

    Refactor : ktlint formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,11 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.ktlint)
+}
+
+allprojects {
+    apply {
+        plugin(rootProject.libs.plugins.ktlint.get().pluginId)
+    }
 }
