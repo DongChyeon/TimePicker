1379ff265aeaf816b07b6454531dbffa13748723
dongchyeon <kimdonghyun90766@gmail.com>
2025-03-30 13:41:28 +0900

    Feat : Implement demo app for TimePicker

--- app/src/main/java/com/dongchyeon/timepicker/MainActivity.kt
+++ app/src/main/java/com/dongchyeon/timepicker/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.dongchyeon.timepicker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.dongchyeon.timepicker.ui.theme.TimePickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TimePickerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = Color.Black),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(20.dp)
+                    ) {
+                        TimePicker(
+                            timeFormat = TimeFormat.TWENTY_FOUR_HOUR
+                        ) { newTime ->
+                            Log.d("TimePicker", "Selected Time: $newTime")
+                        }
+
+                        TimePicker(
+                            selector = TimePickerDefaults.pickerSelector(
+                                color = Color.Gray.copy(alpha = 0.4f),
+                                shape = RoundedCornerShape(16.dp),
+                                border = BorderStroke(1.dp, Color.Gray)
+                            )
+                        ) { newTime ->
+                            Log.d("TimePicker", "Selected Time: $newTime")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TimePickerTheme {
-        Greeting("Android")
     }
 }
